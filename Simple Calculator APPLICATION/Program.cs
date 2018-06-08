1b42b99f2b85ca2f9e7b3e3f88d5a1e2e26c9694
chris.sabaoth <chris.sabaoth@protonmail.com>
2018-06-09 00:13:31 +0100

    First commit

---
+++ Simple Calculator APPLICATION/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator_APPLICATION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+
+            }
+            catch (Exception ex)
+            {
+                //In The Real World I'd Log These Type Of Errors Into A Database
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
